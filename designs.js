cbf29d0d99e45b737e67f6df21c60d610be5227e
Mostafa Medht Abdellah <mostafamedht98@gmail.com>
2018-04-08 20:59:30 +0200

    modifying color fun function to work

--- designs.js
+++ designs.js
@@ -18,42 +18,58 @@ function makeGrid() {
     // Const Var 
     const inputHeight = document.getElementById('input_height').value;
     const inputWidth = document.getElementById('input_width').value;
+    const colorPiked = document.getElementById('colorPicker').value;
+    
+    let addEvent = function(cell) {
+        cell.addEventListener('click', function() {
+            cell.style.backgroundColor = colorPiked;
+        });
+    }
 
+
+    let removeEvent = function(cell) {
+        cell.addEventListener('dblclick', function() {
+            cell.style.backgroundColor = '';
+        });
+    }
     // Creating cell 
     for (let i=0 ; i<inputHeight ;i++){
         var row = document.createElement('tr');
-        row.setAttribute('id','tr');
+        row.setAttribute('class','tr');
         for (let j=0; j<inputWidth ;j++){
-           var col = document.createElement('td');
-           col.setAttribute('id','cell');
-           row.appendChild(col);
+           var cell = document.createElement('td');
+           cell.setAttribute('class','cell');
+           row.appendChild(cell);
+           cell.addEventListener('click',addEvent(cell));
+           cell.addEventListener('dblclick',removeEvent(cell));
         }
         table.appendChild(row);
     }
 
 }
 
-form.addEventListener('submit' , function(e){
+const create_table =document.querySelector('#create_table');
+create_table.addEventListener('click' , function(e){
     e.preventDefault();
     makeGrid();
+    
+    // cell.addEventListener("dblclick",function(e){
+    //     let colorPiked = document.getElementById('colorPicker').value;
+    //     cell.style.backgroundColor = '' ;
+    // });
 });
 
 // clear_Btn.addEventListener('submit', function(event) {
 //     event.preventDefault();
-//     document.getElementById('cell').css('backgroundColor', '#ffffff');
+//     document.querySelector('.cell').style.backgroundColor =  '#ffffff';
 //   });
   
 // reset_Btn.addEventListener('click',function() {
-//     table.children().remove();
+//     table.innerHTML = '';
 //   });
 
-
-document.getElementById('cell').addEventListener("click",function(e){
-    let colorPiked = document.getElementById('colorPicker').value;
-    document.getElementById('cell').style['background-color'] = colorPiked;
-});
-document.getElementById('cell').addEventListener("dblclick",function(e){
-    let colorPiked = document.getElementById('colorPicker').value;
-    document.getElementById('cell').style['background-color'] = '' ;
-});
-  
+// let cell = document.querySelector('.cell');  
+// cell.addEventListener("click",function(e){
+//     let colorPiked = document.getElementById('colorPicker').value;
+//     e.target.style.backgroundColor = colorPiked;
+// });
